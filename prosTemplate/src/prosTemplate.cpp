1435dbba8b48756ba1d66d4a03eb78f08d8a12dd
4rmy <hjacob215@gmail.com>
2023-02-28 22:04:32 -0500

    finished auton selector

--- prosTemplate/src/prosTemplate.cpp
+++ prosTemplate/src/prosTemplate.cpp
@@ -4,14 +4,18 @@
 #include "display/lv_core/lv_style.h"
 #include "display/lv_draw/lv_draw_line.h"
 #include "display/lv_draw/lv_draw_rect.h"
+#include "display/lv_fonts/lv_font_builtin.h"
 #include "display/lv_misc/lv_area.h"
 #include "display/lv_misc/lv_color.h"
+#include "display/lv_misc/lv_font.h"
 #include "display/lv_objx/lv_btn.h"
 #include "display/lv_objx/lv_btnm.h"
 #include "display/lv_objx/lv_canvas.h"
+#include "display/lv_objx/lv_img.h"
 #include "display/lv_objx/lv_label.h"
 #include <cstddef>
 #include <iterator>
+#include "242.h"
 
 double targetAngle = 0;
 int leftTurnSpeed = 0;
@@ -119,6 +123,30 @@ lv_res_t atnAction8(lv_obj_t *btn){
 }
 
 void draw() {
+    static lv_style_t style_btn_rel;
+    lv_style_copy(&style_btn_rel, &lv_style_plain);
+    style_btn_rel.body.radius = 10;
+    style_btn_rel.body.border.width = 2;
+    style_btn_rel.body.border.color.alpha = 255;
+    style_btn_rel.body.border.color.red = 255;
+    style_btn_rel.body.border.color.green = 0;
+    style_btn_rel.body.border.color.blue = 0;
+    style_btn_rel.body.main_color.alpha = 255;
+    style_btn_rel.body.main_color.red = 50;
+    style_btn_rel.body.main_color.green = 50;
+    style_btn_rel.body.main_color.blue = 50;
+    style_btn_rel.body.grad_color.full = style_btn_rel.body.main_color.full;
+    style_btn_rel.text.color.full = 0xFFFFFFFF;
+
+    static lv_style_t style_btn_pr;
+    lv_style_copy(&style_btn_pr, &style_btn_rel);
+    style_btn_rel.body.main_color.red = 255;
+    style_btn_rel.body.main_color.green = 255;
+    style_btn_rel.body.main_color.blue = 255;
+    style_btn_rel.body.grad_color.full = style_btn_rel.body.main_color.full;
+    style_btn_rel.text.color.full = 0xFF000000;
+    
+
     lv_obj_t * canvas = lv_canvas_create(lv_scr_act(), NULL);
     lv_obj_set_x(canvas, 0);
     lv_obj_set_y(canvas, 0);
@@ -134,6 +162,8 @@ void draw() {
     lv_obj_set_width(atn1, 75);
     lv_obj_set_height(atn1, 75);
     lv_btn_set_action(atn1, LV_BTN_ACTION_CLICK, *atnAction0);
+    lv_btn_set_style(atn1, LV_BTN_STYLE_PR, &style_btn_rel);
+    lv_btn_set_style(atn1, LV_BTN_STYLE_REL, &style_btn_pr);
     lv_obj_t * atn1lbl = lv_label_create(atn1, NULL);
     lv_label_set_text(atn1lbl, prosTemplate::autons[i].name.c_str());
     lv_label_set_align(atn1lbl, LV_LABEL_ALIGN_CENTER);
@@ -147,6 +177,8 @@ void draw() {
     lv_obj_set_width(atn2, 75);
     lv_obj_set_height(atn2, 75);
     lv_btn_set_action(atn2, LV_BTN_ACTION_CLICK, *atnAction1);
+    lv_btn_set_style(atn2, LV_BTN_STYLE_PR, &style_btn_rel);
+    lv_btn_set_style(atn2, LV_BTN_STYLE_REL, &style_btn_pr);
     lv_obj_t * atn2lbl = lv_label_create(atn2, NULL);
     lv_label_set_text(atn2lbl, prosTemplate::autons[i].name.c_str());
     lv_label_set_align(atn2lbl, LV_LABEL_ALIGN_CENTER);
@@ -160,6 +192,8 @@ void draw() {
     lv_obj_set_width(atn3, 75);
     lv_obj_set_height(atn3, 75);
     lv_btn_set_action(atn3, LV_BTN_ACTION_CLICK, *atnAction2);
+    lv_btn_set_style(atn3, LV_BTN_STYLE_PR, &style_btn_rel);
+    lv_btn_set_style(atn3, LV_BTN_STYLE_REL, &style_btn_pr);
     lv_obj_t * atn3lbl = lv_label_create(atn3, NULL);
     lv_label_set_text(atn3lbl, prosTemplate::autons[i].name.c_str());
     lv_label_set_align(atn3lbl, LV_LABEL_ALIGN_CENTER);
@@ -173,6 +207,8 @@ void draw() {
     lv_obj_set_width(atn4, 75);
     lv_obj_set_height(atn4, 75);
     lv_btn_set_action(atn4, LV_BTN_ACTION_CLICK, *atnAction3);
+    lv_btn_set_style(atn4, LV_BTN_STYLE_PR, &style_btn_rel);
+    lv_btn_set_style(atn4, LV_BTN_STYLE_REL, &style_btn_pr);
     lv_obj_t * atn4lbl = lv_label_create(atn4, NULL);
     lv_label_set_text(atn4lbl, prosTemplate::autons[i].name.c_str());
     lv_label_set_align(atn4lbl, LV_LABEL_ALIGN_CENTER);
@@ -186,6 +222,8 @@ void draw() {
     lv_obj_set_width(atn5, 75);
     lv_obj_set_height(atn5, 75);
     lv_btn_set_action(atn5, LV_BTN_ACTION_CLICK, *atnAction4);
+    lv_btn_set_style(atn5, LV_BTN_STYLE_PR, &style_btn_rel);
+    lv_btn_set_style(atn5, LV_BTN_STYLE_REL, &style_btn_pr);
     lv_obj_t * atn5lbl = lv_label_create(atn5, NULL);
     lv_label_set_text(atn5lbl, prosTemplate::autons[i].name.c_str());
     lv_label_set_align(atn5lbl, LV_LABEL_ALIGN_CENTER);
@@ -199,6 +237,8 @@ void draw() {
     lv_obj_set_width(atn6, 75);
     lv_obj_set_height(atn6, 75);
     lv_btn_set_action(atn6, LV_BTN_ACTION_CLICK, *atnAction5);
+    lv_btn_set_style(atn6, LV_BTN_STYLE_PR, &style_btn_rel);
+    lv_btn_set_style(atn6, LV_BTN_STYLE_REL, &style_btn_pr);
     lv_obj_t * atn6lbl = lv_label_create(atn6, NULL);
     lv_label_set_text(atn6lbl, prosTemplate::autons[i].name.c_str());
     lv_label_set_align(atn6lbl, LV_LABEL_ALIGN_CENTER);
@@ -212,6 +252,8 @@ void draw() {
     lv_obj_set_width(atn7, 75);
     lv_obj_set_height(atn7, 75);
     lv_btn_set_action(atn7, LV_BTN_ACTION_CLICK, *atnAction6);
+    lv_btn_set_style(atn7, LV_BTN_STYLE_PR, &style_btn_rel);
+    lv_btn_set_style(atn7, LV_BTN_STYLE_REL, &style_btn_pr);
     lv_obj_t * atn7lbl = lv_label_create(atn7, NULL);
     lv_label_set_text(atn7lbl, prosTemplate::autons[i].name.c_str());
     lv_label_set_align(atn7lbl, LV_LABEL_ALIGN_CENTER);
@@ -225,6 +267,8 @@ void draw() {
     lv_obj_set_width(atn8, 75);
     lv_obj_set_height(atn8, 75);
     lv_btn_set_action(atn8, LV_BTN_ACTION_CLICK, *atnAction7);
+    lv_btn_set_style(atn8, LV_BTN_STYLE_PR, &style_btn_rel);
+    lv_btn_set_style(atn8, LV_BTN_STYLE_REL, &style_btn_pr);
     lv_obj_t * atn8lbl = lv_label_create(atn8, NULL);
     lv_label_set_text(atn8lbl, prosTemplate::autons[i].name.c_str());
     lv_label_set_align(atn8lbl, LV_LABEL_ALIGN_CENTER);
@@ -232,11 +276,13 @@ void draw() {
     i++;
 
     lv_obj_t * skills = lv_btn_create(canvas, NULL);
-    lv_obj_set_x(skills, 400);
-    lv_obj_set_y(skills, 80);
-    lv_obj_set_width(skills, 75);
-    lv_obj_set_height(skills, 75);
+    lv_obj_set_x(skills, 355);
+    lv_obj_set_y(skills, 135);
+    lv_obj_set_width(skills, 100);
+    lv_obj_set_height(skills, 100);
     lv_btn_set_action(skills, LV_BTN_ACTION_CLICK, *atnAction8);
+    lv_btn_set_style(skills, LV_BTN_STYLE_PR, &style_btn_rel);
+    lv_btn_set_style(skills, LV_BTN_STYLE_REL, &style_btn_pr);
     lv_obj_t * skillslbl = lv_label_create(skills, NULL);
     lv_label_set_text(skillslbl, prosTemplate::autons[i].name.c_str());
     lv_label_set_align(skillslbl, LV_LABEL_ALIGN_CENTER);
